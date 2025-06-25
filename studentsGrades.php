be1bc072f22d894cc0e4fa62cd87f2db11187445
MGT25 <mazhar71726@gmail.com>
2025-06-25 16:33:45 +0500

    remove extra lines

--- studentsGrades.php
+++ studentsGrades.php
@@ -63,8 +63,7 @@
                 $result= $selectstmt->setFetchMode(PDO::FETCH_ASSOC);
                 $exists= $selectstmt->fetchAll();
 
-                if (count($exists)){    
-                    print_r($exists);
+                if (count($exists)){
                     if ($exists[0]['grade'] !== $grade) {
                         $updatestmt = $conn->prepare("UPDATE `students_grades` SET `grade` = :grade WHERE student_id = :student_id AND course_name = :course_name");
                             $updatestmt->bindParam(":student_id", $student_id);
@@ -87,7 +86,6 @@
                         $insertstmt->bindParam(":course_name", $course_name);
                         $insertstmt->bindParam(":grade", $grade);
                         $insertstmt->execute();
-                        echo "inserted successfully";
                     }
                 }
 
